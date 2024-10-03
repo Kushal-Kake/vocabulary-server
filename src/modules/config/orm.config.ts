d8f3ddc5d0e1aa10f83d912648d9426f4b8e3754
Kushal Kake <kushalkumarkake@gmail.com>
2024-10-03 07:48:34 +0530

    refactor: cleanup

--- src/modules/config/orm.config.ts
+++ src/modules/config/orm.config.ts
@@ -2,7 +2,6 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const getOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => {  
-  // MongoDB Configuration (without useUnifiedTopology)
   return {
     type: 'mongodb',
     url: configService.get<string>('mongodb.uri'),
